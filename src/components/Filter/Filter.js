1ad96c65131e1b25193d86ca90ae6f50be27b7de
Serhii Hayvoronskyi <63624245+Mr-Serzh@users.noreply.github.com>
2021-03-27 02:43:58 +0200

    Initial commit

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import s from './Filter.module.css';
+
+function Filter({ value, onChange }) {
+  return (
+    <label className={s.label}>
+      Find contacts by name
+      <input
+        className={s.input}
+        type="text"
+        value={value}
+        onChange={onChange}
+        name="filter"
+        placeholder="input name"
+      />
+    </label>
+  );
+}
+
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
